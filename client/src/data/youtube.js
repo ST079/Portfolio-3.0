72f4871fb9fcee5eb860b61a6c69e9e84e5e33fd
Sujan Tamang <suzanyba079@gmail.com>
2025-08-08 08:28:48 +0545

    updates

--- client/src/data/youtube.js
+++ client/src/data/youtube.js
@@ -5,7 +5,7 @@ export const videos = [
     description:
       "I have explained the basics of the 8085 microprocessor, its architecture, and how to write simple assembly language programs.",
     thumbnail:
-      "https://i9.ytimg.com/vi_webp/u1w-6jXcKyU/mqdefault.webp?v=6884fa57&sqp=CJSky8QG&rs=AOn4CLD_G70Ee919W985m-va1qRVueC2hg",
+      "https://i9.ytimg.com/vi_webp/u1w-6jXcKyU/mqdefault.webp?v=6884fa57&sqp=CNTE1cQG&rs=AOn4CLC9I8QOomiV4mPs1LVNwbRoQtpjtA",
     url: "https://youtu.be/u1w-6jXcKyU?si=AbkQzAjoe8V6lAO8",
   },
   {
@@ -14,7 +14,7 @@ export const videos = [
     description:
       "In this video, I have explained the pin diagram of the 8085 microprocessor and its functions.",
     thumbnail:
-      "https://i9.ytimg.com/vi/jb4znTgtoe4/mqdefault.jpg?v=688ce5bd&sqp=COihy8QG&rs=AOn4CLC2jjKpI7fC8bcVTTqvdroBnjyahw",
+      "https://i9.ytimg.com/vi/jb4znTgtoe4/mqdefault.jpg?v=688ce5bd&sqp=CIDH1cQG&rs=AOn4CLChuoAMAm1s3TkADFOdlkJwO1xnnQ",
     url: "https://www.youtube.com/watch?v=jb4znTgtoe4&t=11s",
   },
   // Add more videos...
